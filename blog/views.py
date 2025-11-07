ff7afde772405491908ea5f1d29ce41597d6e21c
petercodes <ojoiyanuoluwa1999@gmail.com>
2025-11-07 13:49:26 +0100

    time to create a place to store users

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
 def home(request):
-    return render(request, 'blog/home.html')
+    posts = Post.objects.all()
+    return render(request, 'blog/home.html',{'posts':posts})
 
 def about(request):
     return render(request, 'blog/about.html', {'title':'About'})
